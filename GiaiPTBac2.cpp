2fa49aea87ff09f243c8563824ce6d0f7b8692a6
mytuyen83 <huynhthimytuyen2006@gmail.com>
2025-04-03 11:44:25 +0700

    Add files via upload

---
+++ GiaiPTBac2.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+// thiet ke ham
+int giaiptbac2(float a, float b, float c, float& x1, float& x2)
+{
+    int sn = -1;
+    if (a == 0) {
+        if (b == 0) {
+            if (c == 0)
+                sn = -1;
+            else
+                sn = 0;
+        }
+        else { // pt bac 1: bx + c  = 0
+            sn = 1;
+            x1 = -c / b;
+        }
+    }
+    else {
+        float delta = b * b - 4 * a * c;
+        if (delta < 0) sn = 0;
+        else if (delta == 0) {
+            sn = 1;
+            x1 = -b / (2 * a);
+        }
+        else {
+            sn = 2;
+            x1 = (-b - sqrt(delta)) / (2 * a);
+            x2 = (-b + sqrt(delta)) / (2 * a);
+            if (x1 > x2) { // ky thuat cam co
+                float t = x1;
+                x1 = x2;
+                x2 = t;
+            }
+        }
+    }
+    return sn;
+}
+int main() {
+    float a, b, c;
+    int sn;
+    float x1, x2;
+    cin >> a >> b >> c;
+    sn = giaiptbac2(a, b, c, x1, x2);
+    if (sn == -1)
+        cout << "Phuong trinh co vo so nghiem";
+    else if (sn == 0)
+        cout << "Phuong trinh vo nghiem";
+    else {
+        cout << "Phuong trinh co " << sn << " nghiem: " << x1;
+        if (sn == 2);
+        cout << " " << x2;
+    }
+    return 0;
+}
